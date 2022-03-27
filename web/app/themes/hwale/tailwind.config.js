890e3cf144d1da17dbfd59b0c87bf8f8940a2fb4
Guy Whale <guywhale@gmail.com>
2022-03-27 19:06:48 +0100

    setup: add colours

--- web/app/themes/hwale/tailwind.config.js
+++ web/app/themes/hwale/tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   content: ['./app/**/*.php', './resources/**/*.{php,vue,js}'],
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        'black-1': '#252525',
+        'brown-1': '#8C7065',
+        'grey-1': '#b2b2b2',
+        'pink-1': '#f49898', 
+        'red-1': '#f14d4c',
+        'red-2': '#732525',
+        'white-1': '#fff',
+      },
     },
   },
   plugins: [],
