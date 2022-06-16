fc6e57cd5c48c49208c25d35d03447a1bb60936a
Guy Whale <guywhale@gmail.com>
2022-06-16 15:41:13 +0100

    add current page nav menu styles for desktop

--- web/app/themes/hwale/app/navwalker.php
+++ web/app/themes/hwale/app/navwalker.php
@@ -13,19 +13,23 @@ class MainNavWalker extends \Walker_Nav_Menu
         $numberClasses = 'nav-number-sm lg:text-nav-number-lg xl:text-nav-number basis-full max-w-full mb-0.5';
         $numberColour = 'text-grey';
         $interactClasses = 'group-hover:translate-x-0 group-active:translate-x-0 group-focus:translate-x-0 transition-transform';
-        $lineClasses = 'relative left-0 w-3.75 h-0.5 mr-2.5 bg-grey -translate-x-3.75';
-        $titleClasses = 'text-nav-sm lg:text-nav-lg xl:text-nav -translate-x-6.25';
+        $lineColourPosition = 'bg-grey -translate-x-3.75';
+        $lineClasses = 'relative left-0 w-3.75 h-0.5 mr-2.5';
+        $titlePosition = '-translate-x-6.25';
+        $titleClasses = 'text-nav-sm lg:text-nav-lg xl:text-nav';
 
         if ($item->current) {
             $currentAria = 'aria-current="page"';
             $numberColour = 'text-red';
+            $lineColourPosition = 'bg-red translate-x-0';
+            $titlePosition = 'translate-x-0';
         }
 
         $output .= "<li id=\"menu-item-{$item->ID}\" class=\"{$itemClasses}\">";
         $output .= "<a href=\"{$item->url}\" class=\"{$linkClasses}\" {$currentAria}>";
         $output .= "<span class=\"{$numberClasses} {$numberColour}\">0{$item->menu_order}</span>";
-        $output .= "<span class=\"{$lineClasses} {$interactClasses}\"></span>";
-        $output .= "<span class=\"{$titleClasses} {$interactClasses}\">{$item->title}</span>";
+        $output .= "<span class=\"{$lineClasses} {$lineColourPosition} {$interactClasses}\"></span>";
+        $output .= "<span class=\"{$titleClasses} {$titlePosition} {$interactClasses}\">{$item->title}</span>";
 
         if ($item->description != '' && $depth == 0) {
             $output .= "<small class=\"description\">{$item->description}</small>";
