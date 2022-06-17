06b26529203f487d583649d26b05e0e2e104913d
Guy Whale <guywhale@gmail.com>
2022-06-17 11:49:03 +0100

    add footer

--- web/app/themes/hwale/resources/views/page.blade.php
+++ web/app/themes/hwale/resources/views/page.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
   @while(have_posts()) @php(the_post())
-    <x-floating-title />
     @includeFirst(['partials.content-page', 'partials.content'])
   @endwhile
 @endsection
--- web/app/themes/hwale/resources/views/sections/footer.blade.php
+++ web/app/themes/hwale/resources/views/sections/footer.blade.php
@@ -1,3 +1,4 @@
-<footer class="content-info">
-  @php(dynamic_sidebar('sidebar-footer'))
+<footer>
+  <p class="container text-center text-nav-number">&copy {{ date('Y') }} Guy Whale. All rights reserved.</p>
+  {{-- @php(dynamic_sidebar('sidebar-footer')) --}}
 </footer>
