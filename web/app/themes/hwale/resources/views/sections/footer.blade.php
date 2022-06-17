e0034c445dd884c0670214e8b812630a78316ec5
Guy Whale <guywhale@gmail.com>
2022-06-17 12:25:07 +0100

    remove unneeded code from footer

--- web/app/themes/hwale/resources/views/sections/footer.blade.php
+++ web/app/themes/hwale/resources/views/sections/footer.blade.php
@@ -1,4 +1,3 @@
 <footer>
   <p class="container text-center text-nav-number">&copy {{ date('Y') }} Guy Whale. All rights reserved.</p>
-  {{-- @php(dynamic_sidebar('sidebar-footer')) --}}
 </footer>
