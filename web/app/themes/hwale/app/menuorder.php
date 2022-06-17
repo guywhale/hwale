e1e9f4073531599fb53a28d19f72cbdb273e91e7
Guy Whale <guywhale@gmail.com>
2022-06-17 10:52:16 +0100

    add menu order metabox to pages for use in floating page title and nav menu

---
+++ web/app/themes/hwale/app/menuorder.php
@@ -0,0 +1,15 @@
+<?php
+namespace App;
+
+add_action('init', 'App\addMenuOrder');
+
+/**
+ * Add menu order metabox to pages
+ *
+ * @return void
+ **/
+
+function addMenuOrder()
+{
+    add_post_type_support('page', 'page-attributes');
+}
--- web/app/themes/hwale/functions.php
+++ web/app/themes/hwale/functions.php
@@ -54,7 +54,7 @@ try {
 |
 */
 
-collect(['setup', 'filters', 'navwalker'])
+collect(['setup', 'filters', 'navwalker', 'menuorder'])
     ->each(function ($file) {
         if (! locate_template($file = "app/{$file}.php", true, true)) {
             wp_die(
