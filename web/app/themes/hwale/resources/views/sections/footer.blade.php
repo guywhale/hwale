cda33a4ad706e1f2b67f3bddd038b38cf4551041
Guy Whale <guywhale@gmail.com>
2022-06-23 14:30:40 +0100

    increase z-index and decrease opacity of footer so it's visible on work page but isn't too prominent

--- web/app/themes/hwale/resources/views/sections/footer.blade.php
+++ web/app/themes/hwale/resources/views/sections/footer.blade.php
@@ -1,3 +1,3 @@
-<footer class="py-6 -mt-14.5">
+<footer class="relative py-6 -mt-14.5 opacity-80">
   <p class="container text-center text-grey text-nav-number">&copy {{ date('Y') }} Guy Whale. All rights reserved.</p>
 </footer>
