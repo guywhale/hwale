6969b028d4c88ca4a9d79e6928dc845395978929
Guy Whale <guywhale@gmail.com>
2022-06-24 10:18:07 +0100

    set up swiper and remove snap references

--- web/app/themes/hwale/resources/views/blocks/work-slider.blade.php
+++ web/app/themes/hwale/resources/views/blocks/work-slider.blade.php
@@ -3,11 +3,11 @@
     <div class="relative flex items-center justify-center w-full h-1/3-screen bg-red-dark md:h-1/2-screen lg:h-screen lg:w-1/2 animate-slide-in-left">
       <x-laptop-image :laptopImage="$laptopImage" />
       @if ($work['images'])
-        <div class="relative -top-[7px] left-px w-[432px] h-[270px] bg-pink overflow-x-hidden overflow-y-scroll snap-y snap-mandatory">
+        <div class="relative -top-[7px] left-px w-[432px] h-[270px] bg-pink">
           @foreach ($work['images'] as $slideNumber => $image)
           <img src="{{ $image['image'] }}"
             alt=""
-            class="object-cover w-full h-full snap-start snap-always"
+            class="object-cover w-full h-full"
           >
           {{-- @dump($slideNumber)
           @dump($image) --}}
@@ -15,7 +15,7 @@
         </div>
       @endif
     </div>
-    <div class="flex flex-wrap items-center justify-center lg:w-1/2 w-full h-2/3-screen md:h-1/2-screen lg:h-screen overflow-y-scroll snap-y snap-mandatory px-7.5 animate-slide-in-right">
+    <div class="flex flex-wrap items-center justify-center lg:w-1/2 w-full h-2/3-screen md:h-1/2-screen lg:h-screen px-7.5 animate-slide-in-right">
       @if ($work['content'])
         @foreach ($work['content'] as $slideNumber => $content)
           {{-- @dump($slideNumber) --}}
