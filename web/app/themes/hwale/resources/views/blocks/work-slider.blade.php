503fbb002e2758c9da0ada75b0a5229da14e92f2
Guy Whale <guywhale@gmail.com>
2022-06-25 19:45:38 +0100

    Revert "redesign of work page". Bring work slider back to original design but incorporate container and improved tablet design.
    This reverts commit 9f238d34b45465dc0a6f7e8e3c4f6658ec313fca.

--- web/app/themes/hwale/resources/views/blocks/work-slider.blade.php
+++ web/app/themes/hwale/resources/views/blocks/work-slider.blade.php
@@ -1,14 +1,14 @@
 <div class="{{ $block->classes }} relative overflow-x-hidden">
-  <div class="absolute top-0 left-0 right-0 hidden w-full md:block lg:left-unset h-1/2-screen lg:w-1/2 lg:h-full -z-10 bg-red-dark animate-slide-in-right"></div>
+  <div class="absolute top-0 left-0 right-0 hidden w-full md:block lg:right-unset h-1/2-screen lg:w-1/2 lg:h-full -z-10 bg-red-dark animate-slide-in-left"></div>
   <div class="container">
     <div class="flex-col hidden w-full md:flex lg:flex-row">
-      <div class="lg:order-2 relative flex items-center justify-end w-full h-1/3-screen md:h-1/2-screen lg:h-screen lg:w-1/2 animate-slide-in-right lg:flex-row px-3.75 md:px-7.5 lg:pl-7.5 lg:pr-0 xl:pl-15">
+      <div class="relative flex items-center justify-end w-full h-1/3-screen md:h-1/2-screen lg:h-screen lg:w-1/2 animate-slide-in-left lg:flex-row px-3.75 md:px-7.5 lg:pr-7.5 lg:pl-0 xl:pr-15">
         <x-laptop-image :laptopImage="$laptopImage"
           :imageSlides="$work['images']"
         />
       </div>
 
-      <div class="relative flex flex-wrap items-center w-full lg:order-1 swiper lg:w-1/2 md:pr-15 h-1/2-screen lg:h-screen md:pt-11 lg:pr-15 lg:pl-0 lg:pt-5 xl:pt-0 2xl:pr-40 animate-slide-in-left"
+      <div class="relative flex flex-wrap items-center w-full swiper lg:w-1/2 md:pr-15 h-1/2-screen lg:h-screen md:pt-11 lg:pl-15 lg:pr-0 lg:pt-5 xl:pt-0 2xl:pl-40 animate-slide-in-right"
         data-content-slider
       >
         @if ($work['content'])
