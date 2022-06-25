04c657be41434c70428e0de24c6e6e620080f1d3
Guy Whale <guywhale@gmail.com>
2022-06-25 09:54:50 +0100

    increase h1 z-index so it appears over red bg and add slide in left animation to slide controls

--- web/app/themes/hwale/resources/views/components/floating-title.blade.php
+++ web/app/themes/hwale/resources/views/components/floating-title.blade.php
@@ -1,4 +1,4 @@
-<h1 class="absolute top-40 {{ $positionOnMobile }} lg:top-1/3-screen lg:right-15 lg:left-unset opacity-0 animate-rotate-3d-x
+<h1 class="absolute top-40 {{ $positionOnMobile }} z-10 pointer-events-none lg:top-1/3-screen lg:right-15 lg:left-unset opacity-0 animate-rotate-3d-x
   {{ $cookieStatus ? '' : 'animate-delay-8s' }}"
 >
   <span class="absolute -top-7 md:-top-8 -left-3 lg:-top-32.5 lg:-left-12 text-xl md:text-h1-number-md lg:text-h1-number">{{ $pageNumber }}</span>
