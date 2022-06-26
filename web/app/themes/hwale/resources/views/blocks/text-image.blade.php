ed65c1cee5d606ba33844a571c10cabb84c89111
Guy Whale <guywhale@gmail.com>
2022-06-26 02:11:28 +0100

    add parallax to text image block image

--- web/app/themes/hwale/resources/views/blocks/text-image.blade.php
+++ web/app/themes/hwale/resources/views/blocks/text-image.blade.php
@@ -3,7 +3,7 @@
     <div class="flex flex-col lg:flex-row">
         <div class="order-2 pt-11 md:pt-16 lg:pt-0 xl:pt-10 2xl:pt-24 lg:w-2/3 xl:w-1/2 lg:order-1">
           @if ($text)
-            <div class="cms animate-slide-in-left">
+            <div class="relative z-10 cms animate-slide-in-left">
               {!! $text !!}
             </div>
           @endif
@@ -17,7 +17,10 @@
           @endif
         </div>
         @if ($image)
-          <picture class="order-1 opacity-0 animate-picture-in lg:order-2 lg:w-1/3 xl:w-1/2 xl:pl-[8%]">
+          <picture class="order-1 opacity-0 animate-picture-in lg:order-2 lg:w-1/3 xl:w-1/2 xl:pl-[8%]"
+            x-data
+            @scroll.window="$el.style.transform = `translateY(${window.scrollY * 0.2}px)`;"
+          >
             <source media="(max-width: 1023px)"
               srcset="{{ $image['sizes']['medium'] }} {{ $image['sizes']['medium-width'] }}w"
               sizes="(max-width: 1023px) {{ $image['sizes']['medium-width'] }}"
--- web/app/themes/hwale/resources/views/components/floating-title.blade.php
+++ web/app/themes/hwale/resources/views/components/floating-title.blade.php
@@ -1,13 +1,12 @@
 
 <span class="h1-wrapper fixed {{ $position }} z-10 pointer-events-none block"
   x-data
-  @scroll.window="$el.style.transform = `translateY(calc(${window.scrollY * -0.2}px))`;"
+  @scroll.window="$el.style.transform = `translateY(${window.scrollY * -0.4}px)`;"
 >
   <h1 class=" opacity-0 transition-transform animate-rotate-3d-x
     {{ $cookieStatus ? '' : 'animate-delay-8s' }}"
     data-floating-title
-    {{-- x-data --}}
-    >
+  >
     <span class="absolute -top-7 md:-top-8 -left-3 lg:-top-32.5 lg:-left-12 text-xl md:text-h1-number-md lg:text-h1-number">{{ $pageNumber }}</span>
     {{ $pageTitle }}
   </h1>
