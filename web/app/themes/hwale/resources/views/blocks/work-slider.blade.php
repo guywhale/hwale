b059c57d8ca72686829686364afde56ea5bcd8c8
Guy Whale <guywhale@gmail.com>
2022-06-26 02:35:07 +0100

    center laptop on tablet and make slider controls flush with edge of container

--- web/app/themes/hwale/resources/views/blocks/work-slider.blade.php
+++ web/app/themes/hwale/resources/views/blocks/work-slider.blade.php
@@ -2,7 +2,7 @@
   <div class="absolute top-0 left-0 right-0 hidden w-full md:block lg:right-unset h-1/2-screen lg:w-1/2 lg:h-full -z-10 bg-red-dark animate-slide-in-left"></div>
   <div class="container">
     <div class="flex-col hidden w-full md:flex lg:flex-row">
-      <div class="relative flex items-center justify-end w-full h-1/3-screen md:h-1/2-screen lg:h-screen lg:w-1/2 animate-slide-in-left lg:flex-row px-3.75 md:px-7.5 lg:pr-7.5 lg:pl-0 xl:pr-15">
+      <div class="relative flex items-center md:justify-center lg:justify-end w-full h-1/3-screen md:h-1/2-screen lg:h-screen lg:w-1/2 animate-slide-in-left lg:flex-row px-3.75 md:px-7.5 lg:pr-7.5 lg:pl-0 xl:pr-15">
         <x-laptop-image :laptopImage="$laptopImage"
           :imageSlides="$work['images']"
         />
