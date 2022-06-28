6a702a8ab4b5e0a807eab6064a577d53c9c60e59
Guy Whale <guywhale@gmail.com>
2022-06-28 15:11:25 +0100

    add aria labels to slider controls

--- web/app/themes/hwale/resources/views/components/slider-controls.blade.php
+++ web/app/themes/hwale/resources/views/components/slider-controls.blade.php
@@ -1,9 +1,13 @@
 <div class="absolute z-10 flex flex-col items-center right-0 lg:right-unset lg:left-3.75 xl:left-7.5 2xl:left-16.25 opacity-0 animate-slide-in-left animate-delay-300">
-  <button class="prev-slide">
+  <button class="prev-slide"
+    aria-label="previous project"
+  >
     {!! get_svg('images.prev-arrow', 'w-7.5') !!}
   </button>
   <div class="my-2 leading-none text-center text-body-small slider-pagination"></div>
-  <button class="next-slide">
+  <button class="next-slide"
+    aria-label="next project"
+  >
     {!! get_svg('images.next-arrow', 'w-7.5') !!}
   </button>
 </div>
