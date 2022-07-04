1db187cad9ea9e3a9551e2ef413607673a64d2a9
Guy Whale <guywhale@gmail.com>
2022-07-04 17:08:13 +0100

    add slide in animations to search results page

--- web/app/themes/hwale/resources/views/search.blade.php
+++ web/app/themes/hwale/resources/views/search.blade.php
@@ -5,8 +5,12 @@
 
   <div class="min-h-screen pb-40 pt-68 md:pt-75 lg:pt-32 lg:pb-56">
     <div class="container">
-      <x-search-text />
-      <x-search-form />
+      <div class="opacity-0 animate-slide-in-left">
+        <x-search-text />
+      </div>
+      <div class="opacity-0 animate-slide-in-left animate-delay-300">
+        <x-search-form />
+      </div>
 
       @while(have_posts()) @php(the_post())
         <x-search-card />
